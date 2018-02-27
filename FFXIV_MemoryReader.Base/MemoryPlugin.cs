3a83567d6cd68c07967cc6c73e56d36b5489bfa0
TamanegiMage <31086738+TamanegiMage@users.noreply.github.com>
2018-02-27 14:25:05 +0900

    fix typo

--- FFXIV_MemoryReader.Base/MemoryPlugin.cs
+++ FFXIV_MemoryReader.Base/MemoryPlugin.cs
@@ -22,7 +22,7 @@ namespace TamanegiMage.FFXIV_MemoryReader.Base
             Core.Init(pluginScreenSpace, pluginStatusText);
         }
 
-        public List<Model.CombatantV1> GetCombatantsV1() => Core.GetConbatantsV1();
+        public List<Model.CombatantV1> GetCombatantsV1() => Core.GetCombatantsV1();
         public Model.CameraInfoV1 GetCameraInfoV1() => Core.GetCameraInfoV1();
 
     }
