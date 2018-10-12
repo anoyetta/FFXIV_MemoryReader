ad19848129a07729fbc4be5e61719da4067610a1
TamanegiMage <31086738+TamanegiMage@users.noreply.github.com>
2018-10-12 22:50:38 +0900

    fix signature of CameraInfo

--- FFXIV_MemoryReader.Core/MemoryScanner.cs
+++ FFXIV_MemoryReader.Core/MemoryScanner.cs
@@ -47,7 +47,7 @@ namespace TamanegiMage.FFXIV_MemoryReader.Core
                 PointerType.CameraInfo,
                 new Pointer
                 {
-                    Signature = "10000000788301000000000000000000??????0000000000FFFFFFFF00000000",
+                    Signature = "10000000788301000000000000000000????????00000000FFFFFFFF00000000",
                     PointerPath = new long[0],
                     Architecture = Architecture.x64,
                 }
